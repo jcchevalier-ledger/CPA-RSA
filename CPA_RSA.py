796003d3f38bc9686fc926700284465864d486a8
Jean-Christophe <jean.christophe.chevalier.3@gmail.com>
2020-01-22 16:12:03 +0100

    update: added documentation
    Signed-off-by: Jean-Christophe <jean.christophe.chevalier.3@gmail.com>

--- CPA_RSA.py
+++ CPA_RSA.py
@@ -2,6 +2,11 @@ import numpy as np
 
 
 def read_curve(curve_index):
+    """
+    This function reads the .txt files containing the consumption' curves
+    :param curve_index: the index of the curve
+    :return: a list containing the curve's data
+    """
     f = open("data/curve_" + str(curve_index) + ".txt", "r")
     data = str.split(f.read())
     data = [float(data[j]) for j in range(28)]
@@ -9,12 +14,21 @@ def read_curve(curve_index):
 
 
 def read_message(message_index):
+    """
+    This function reads the .txt files containing the messages
+    :param message_index: the index of the message
+    :return: an integer (the message)
+    """
     f = open("data/msg_" + str(message_index) + ".txt", "r")
     data = int(f.read())
     return data
 
 
 def get_n():
+    """
+    This function reads the .txt file containing the prime number necessary to communicate with a RSA algorithm
+    :return: a integer (the prime number)
+    """
     f = open("data/N.txt", "r")
     data = int(f.read())
     return data
